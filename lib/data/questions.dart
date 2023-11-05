15bb3fc87c5a9ef3ed5dd87d823ddc902e77e06f
Elif Özcan <ozcann159mail.com>
2023-11-05 22:31:34 +0300

    'workshopQuizApp'

---
+++ lib/data/questions.dart
@@ -0,0 +1,14 @@
+import 'package:introapp/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion("Flutter is an open-source UI software development kit created by ______ ",
+  ["Apple", "Google", "Facebook", "Microsoft"]),
+
+  QuizQuestion("When google release Flutter?", 
+  ["Jun 2017", "Jun 2017", "May 2017", "May 2018"]),
+
+  QuizQuestion("A memory location that holds a single letter or number.",
+   ["Double", "Int", "Char", "Word"]),
+
+   
+];
